1a58ebdf747bda2668857c7b52c38bbfca07d0e8
NguyenVH01 <nguyenvuhoangwork@gmail.com>
2024-09-05 03:16:29 +0700

    update domain adaptation

--- src/models/mamba/classification/models/vmamba.py
+++ src/models/mamba/classification/models/vmamba.py
@@ -5,7 +5,7 @@ import copy
 from functools import partial
 from typing import Optional, Callable, Any
 from collections import OrderedDict
-
+from reverse_layer import ReverseLayerF
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -1503,6 +1503,363 @@ class VSSM(nn.Module):
             x = layer(x)
         x = self.classifier(x)
         return x
+
+    def flops(self, shape=(3, 224, 224), verbose=True):
+        # shape = self.__input_shape__[1:]
+        supported_ops={
+            "aten::silu": None, # as relu is in _IGNORED_OPS
+            "aten::neg": None, # as relu is in _IGNORED_OPS
+            "aten::exp": None, # as relu is in _IGNORED_OPS
+            "aten::flip": None, # as permute is in _IGNORED_OPS
+            # "prim::PythonOp.CrossScan": None,
+            # "prim::PythonOp.CrossMerge": None,
+            "prim::PythonOp.SelectiveScanCuda": partial(selective_scan_flop_jit, backend="prefixsum", verbose=verbose),
+        }
+
+        model = copy.deepcopy(self)
+        model.cuda().eval()
+
+        input = torch.randn((1, *shape), device=next(model.parameters()).device)
+        params = parameter_count(model)[""]
+        Gflops, unsupported = flop_count(model=model, inputs=(input,), supported_ops=supported_ops)
+
+        del model, input
+        return sum(Gflops.values()) * 1e9
+        return f"params {params} GFLOPs {sum(Gflops.values())}"
+
+    # used to load ckpt from previous training code
+    def _load_from_state_dict(self, state_dict, prefix, local_metadata, strict, missing_keys, unexpected_keys, error_msgs):
+
+        def check_name(src, state_dict: dict = state_dict, strict=False):
+            if strict:
+                if prefix + src in list(state_dict.keys()):
+                    return True
+            else:
+                key = prefix + src
+                for k in list(state_dict.keys()):
+                    if k.startswith(key):
+                        return True
+            return False
+
+        def change_name(src, dst, state_dict: dict = state_dict, strict=False):
+            if strict:
+                if prefix + src in list(state_dict.keys()):
+                    state_dict[prefix + dst] = state_dict[prefix + src]
+                    state_dict.pop(prefix + src)
+            else:
+                key = prefix + src
+                for k in list(state_dict.keys()):
+                    if k.startswith(key):
+                        new_k = prefix + dst + k[len(key):]
+                        state_dict[new_k] = state_dict[k]
+                        state_dict.pop(k)
+
+        if check_name("pos_embed", strict=True):
+            srcEmb: torch.Tensor = state_dict[prefix + "pos_embed"]
+            state_dict[prefix + "pos_embed"] = F.interpolate(srcEmb.float(), size=self.pos_embed.shape[2:4], align_corners=False, mode="bicubic").to(srcEmb.device)
+
+        change_name("patch_embed.proj", "patch_embed.0")
+        change_name("patch_embed.norm", "patch_embed.2")
+        for i in range(100):
+            for j in range(100):
+                change_name(f"layers.{i}.blocks.{j}.ln_1", f"layers.{i}.blocks.{j}.norm")
+                change_name(f"layers.{i}.blocks.{j}.self_attention", f"layers.{i}.blocks.{j}.op")
+        change_name("norm", "classifier.norm")
+        change_name("head", "classifier.head")
+
+        return super()._load_from_state_dict(state_dict, prefix, local_metadata, strict, missing_keys, unexpected_keys, error_msgs)
+
+class VSSM_Adaptation(nn.Module):
+    def __init__(
+        self, 
+        patch_size=4, 
+        in_chans=3, 
+        num_classes=3, 
+        domain_classes=2,
+        depths=[2, 2, 5, 2], 
+        dims=[96, 192, 384, 768], 
+        # =========================
+        ssm_d_state=16,
+        ssm_ratio=2.0,
+        ssm_dt_rank="auto",
+        ssm_act_layer="silu",        
+        ssm_conv=3,
+        ssm_conv_bias=True,
+        ssm_drop_rate=0.0, 
+        ssm_init="v0",
+        forward_type="v2",
+        # =========================
+        mlp_ratio=4.0,
+        mlp_act_layer="gelu",
+        mlp_drop_rate=0.0,
+        gmlp=False,
+        # =========================
+        drop_path_rate=0.1, 
+        patch_norm=True, 
+        norm_layer="LN", # "BN", "LN2D"
+        downsample_version: str = "v2", # "v1", "v2", "v3"
+        patchembed_version: str = "v1", # "v1", "v2"
+        use_checkpoint=False,  
+        # =========================
+        posembed=False,
+        imgsize=224,
+        _SS2D=SS2D,
+        # =========================
+        **kwargs,
+    ):
+        super().__init__()
+        self.channel_first = (norm_layer.lower() in ["bn", "ln2d"])
+        self.num_classes = num_classes
+        self.domain_classes = domain_classes
+        self.num_layers = len(depths)
+        if isinstance(dims, int):
+            dims = [int(dims * 2 ** i_layer) for i_layer in range(self.num_layers)]
+        self.num_features = dims[-1]
+        self.dims = dims
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))]  # stochastic depth decay rule
+        
+        _NORMLAYERS = dict(
+            ln=nn.LayerNorm,
+            ln2d=LayerNorm2d,
+            bn=nn.BatchNorm2d,
+        )
+
+        _ACTLAYERS = dict(
+            silu=nn.SiLU, 
+            gelu=nn.GELU, 
+            relu=nn.ReLU, 
+            sigmoid=nn.Sigmoid,
+        )
+
+        norm_layer: nn.Module = _NORMLAYERS.get(norm_layer.lower(), None)
+        ssm_act_layer: nn.Module = _ACTLAYERS.get(ssm_act_layer.lower(), None)
+        mlp_act_layer: nn.Module = _ACTLAYERS.get(mlp_act_layer.lower(), None)
+
+        self.pos_embed = self._pos_embed(dims[0], patch_size, imgsize) if posembed else None
+
+        _make_patch_embed = dict(
+            v1=self._make_patch_embed, 
+            v2=self._make_patch_embed_v2,
+        ).get(patchembed_version, None)
+        self.patch_embed = _make_patch_embed(in_chans, dims[0], patch_size, patch_norm, norm_layer, channel_first=self.channel_first)
+
+        _make_downsample = dict(
+            v1=PatchMerging2D, 
+            v2=self._make_downsample, 
+            v3=self._make_downsample_v3, 
+            none=(lambda *_, **_k: None),
+        ).get(downsample_version, None)
+
+        self.layers = nn.ModuleList()
+        for i_layer in range(self.num_layers):
+            downsample = _make_downsample(
+                self.dims[i_layer], 
+                self.dims[i_layer + 1], 
+                norm_layer=norm_layer,
+                channel_first=self.channel_first,
+            ) if (i_layer < self.num_layers - 1) else nn.Identity()
+
+            self.layers.append(self._make_layer(
+                dim = self.dims[i_layer],
+                drop_path = dpr[sum(depths[:i_layer]):sum(depths[:i_layer + 1])],
+                use_checkpoint=use_checkpoint,
+                norm_layer=norm_layer,
+                downsample=downsample,
+                channel_first=self.channel_first,
+                # =================
+                ssm_d_state=ssm_d_state,
+                ssm_ratio=ssm_ratio,
+                ssm_dt_rank=ssm_dt_rank,
+                ssm_act_layer=ssm_act_layer,
+                ssm_conv=ssm_conv,
+                ssm_conv_bias=ssm_conv_bias,
+                ssm_drop_rate=ssm_drop_rate,
+                ssm_init=ssm_init,
+                forward_type=forward_type,
+                # =================
+                mlp_ratio=mlp_ratio,
+                mlp_act_layer=mlp_act_layer,
+                mlp_drop_rate=mlp_drop_rate,
+                gmlp=gmlp,
+                # =================
+                _SS2D=_SS2D,
+            ))
+
+        ###
+        # Classes Classifier 
+        ###
+        self.classifier = nn.Sequential(OrderedDict(
+            norm=norm_layer(self.num_features), # B,H,W,C
+            permute=(Permute(0, 3, 1, 2) if not self.channel_first else nn.Identity()),
+            avgpool=nn.AdaptiveAvgPool2d(1),
+            flatten=nn.Flatten(1),
+            head=nn.Linear(self.num_features, self.num_classes),
+        ))
+
+        print('Logging')
+        ###
+        # Domain Classifier 
+        ###
+        self.domain_classifier = nn.Sequential(OrderedDict(
+            norm=norm_layer(self.num_features), # B,H,W,C
+            permute=(Permute(0, 3, 1, 2) if not self.channel_first else nn.Identity()),
+            avgpool=nn.AdaptiveAvgPool2d(1),
+            flatten=nn.Flatten(1),
+            d_fc1=nn.Linear(self.num_features, 100),
+            d_bn1=nn.BatchNorm1d(100),
+            d_relu1=nn.ReLU(True),
+            d_fc2=nn.Linear(100, self.domain_classes),
+            d_softmax=nn.LogSoftmax(dim=1)
+        ))
+
+        self.apply(self._init_weights)
+
+    @staticmethod
+    def _pos_embed(embed_dims, patch_size, img_size):
+        patch_height, patch_width = (img_size // patch_size, img_size // patch_size)
+        pos_embed = nn.Parameter(torch.zeros(1, embed_dims, patch_height, patch_width))
+        trunc_normal_(pos_embed, std=0.02)
+        return pos_embed
+
+    def _init_weights(self, m: nn.Module):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    # used in building optimizer
+    @torch.jit.ignore
+    def no_weight_decay(self):
+        return {"pos_embed"}
+
+    # used in building optimizer
+    @torch.jit.ignore
+    def no_weight_decay_keywords(self):
+        return {}
+
+    @staticmethod
+    def _make_patch_embed(in_chans=3, embed_dim=96, patch_size=4, patch_norm=True, norm_layer=nn.LayerNorm, channel_first=False):
+        # if channel first, then Norm and Output are both channel_first
+        return nn.Sequential(
+            nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=patch_size, bias=True),
+            (nn.Identity() if channel_first else Permute(0, 2, 3, 1)),
+            (norm_layer(embed_dim) if patch_norm else nn.Identity()),
+        )
+
+    @staticmethod
+    def _make_patch_embed_v2(in_chans=3, embed_dim=96, patch_size=4, patch_norm=True, norm_layer=nn.LayerNorm, channel_first=False):
+        # if channel first, then Norm and Output are both channel_first
+        stride = patch_size // 2
+        kernel_size = stride + 1
+        padding = 1
+        return nn.Sequential(
+            nn.Conv2d(in_chans, embed_dim // 2, kernel_size=kernel_size, stride=stride, padding=padding),
+            (nn.Identity() if (channel_first or (not patch_norm)) else Permute(0, 2, 3, 1)),
+            (norm_layer(embed_dim // 2) if patch_norm else nn.Identity()),
+            (nn.Identity() if (channel_first or (not patch_norm)) else Permute(0, 3, 1, 2)),
+            nn.GELU(),
+            nn.Conv2d(embed_dim // 2, embed_dim, kernel_size=kernel_size, stride=stride, padding=padding),
+            (nn.Identity() if channel_first else Permute(0, 2, 3, 1)),
+            (norm_layer(embed_dim) if patch_norm else nn.Identity()),
+        )
+    
+    @staticmethod
+    def _make_downsample(dim=96, out_dim=192, norm_layer=nn.LayerNorm, channel_first=False):
+        # if channel first, then Norm and Output are both channel_first
+        return nn.Sequential(
+            (nn.Identity() if channel_first else Permute(0, 3, 1, 2)),
+            nn.Conv2d(dim, out_dim, kernel_size=2, stride=2),
+            (nn.Identity() if channel_first else Permute(0, 2, 3, 1)),
+            norm_layer(out_dim),
+        )
+
+    @staticmethod
+    def _make_downsample_v3(dim=96, out_dim=192, norm_layer=nn.LayerNorm, channel_first=False):
+        # if channel first, then Norm and Output are both channel_first
+        return nn.Sequential(
+            (nn.Identity() if channel_first else Permute(0, 3, 1, 2)),
+            nn.Conv2d(dim, out_dim, kernel_size=3, stride=2, padding=1),
+            (nn.Identity() if channel_first else Permute(0, 2, 3, 1)),
+            norm_layer(out_dim),
+        )
+
+    @staticmethod
+    def _make_layer(
+        dim=96, 
+        drop_path=[0.1, 0.1], 
+        use_checkpoint=False, 
+        norm_layer=nn.LayerNorm,
+        downsample=nn.Identity(),
+        channel_first=False,
+        # ===========================
+        ssm_d_state=16,
+        ssm_ratio=2.0,
+        ssm_dt_rank="auto",       
+        ssm_act_layer=nn.SiLU,
+        ssm_conv=3,
+        ssm_conv_bias=True,
+        ssm_drop_rate=0.0, 
+        ssm_init="v0",
+        forward_type="v2",
+        # ===========================
+        mlp_ratio=4.0,
+        mlp_act_layer=nn.GELU,
+        mlp_drop_rate=0.0,
+        gmlp=False,
+        # ===========================
+        _SS2D=SS2D,
+        **kwargs,
+    ):
+        # if channel first, then Norm and Output are both channel_first
+        depth = len(drop_path)
+        blocks = []
+        for d in range(depth):
+            blocks.append(VSSBlock(
+                hidden_dim=dim, 
+                drop_path=drop_path[d],
+                norm_layer=norm_layer,
+                channel_first=channel_first,
+                ssm_d_state=ssm_d_state,
+                ssm_ratio=ssm_ratio,
+                ssm_dt_rank=ssm_dt_rank,
+                ssm_act_layer=ssm_act_layer,
+                ssm_conv=ssm_conv,
+                ssm_conv_bias=ssm_conv_bias,
+                ssm_drop_rate=ssm_drop_rate,
+                ssm_init=ssm_init,
+                forward_type=forward_type,
+                mlp_ratio=mlp_ratio,
+                mlp_act_layer=mlp_act_layer,
+                mlp_drop_rate=mlp_drop_rate,
+                gmlp=gmlp,
+                use_checkpoint=use_checkpoint,
+                _SS2D=_SS2D,
+            ))
+        
+        return nn.Sequential(OrderedDict(
+            blocks=nn.Sequential(*blocks,),
+            downsample=downsample,
+        ))
+
+    def forward(self, x: torch.Tensor, alpha: float):
+        x = self.patch_embed(x)
+        
+        if self.pos_embed is not None:
+            pos_embed = self.pos_embed.permute(0, 2, 3, 1) if not self.channel_first else self.pos_embed
+            x = x + pos_embed
+        for layer in self.layers:
+            feature = layer(x)
+
+        feature = feature.view(-1, 768)
+        reverse_feature = ReverseLayerF.apply(feature, alpha)
+        class_output = self.classifier(feature)
+        domain_output = self.domain_classifier(reverse_feature)
+
+        # x = self.classifier(x)
+        return class_output, domain_output
 
     def flops(self, shape=(3, 224, 224), verbose=True):
         # shape = self.__input_shape__[1:]
